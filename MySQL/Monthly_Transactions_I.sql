d882d83d6c6d6b6640cfebc34cc569aca9f72fd3
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-07-04 18:59:40 +0530

    Create Monthly_Transactions_I.sql

---
+++ MySQL/Monthly_Transactions_I.sql
@@ -0,0 +1,11 @@
+-- 1193. Monthly Transactions I
+
+SELECT
+    DATE_FORMAT(trans_date, "%Y-%m") as month,
+    country,
+    COUNT(*) as trans_count,
+    COUNT(CASE WHEN state = 'approved' THEN 1 END) as approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month, country;
