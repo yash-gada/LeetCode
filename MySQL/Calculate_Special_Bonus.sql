2e3b8e24fa59d9c59ea4251824cbbdc3195fd205
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-07-10 19:35:19 +0530

    Create Calculate_Special_Bonus.sql

---
+++ MySQL/Calculate_Special_Bonus.sql
@@ -0,0 +1,10 @@
+-- 1873. Calculate Special Bonus
+
+SELECT
+    employee_id,
+    CASE
+        WHEN employee_id % 2 !=0 AND name NOT LIKE 'M%' THEN salary
+        ELSE 0
+    END as bonus
+FROM Employees
+ORDER BY employee_id
