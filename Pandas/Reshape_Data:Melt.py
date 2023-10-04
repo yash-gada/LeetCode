8f959ed75e9189293323de17e185f1e84b9b0707
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-10-04 19:21:02 +0530

    Create Reshape_Data:Melt.py

---
+++ Pandas/Reshape_Data:Melt.py
@@ -0,0 +1,6 @@
+# 2890. Reshape Data: Melt
+
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars="product", var_name="quarter", value_name="sales")
