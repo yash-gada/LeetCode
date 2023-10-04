6a4dbd0d9498713a5cd175ecec115d98b4416e93
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-10-04 19:21:29 +0530

    Create Method_Chaining.py

---
+++ Pandas/Method_Chaining.py
@@ -0,0 +1,6 @@
+# 2891. Method Chaining
+
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    return animals[animals["weight"]>100].sort_values(by="weight", ascending=False)[["name"]]
