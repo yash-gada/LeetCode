bae332a2b48be2bd22ba04f1d401b431b44a50b8
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-10-04 19:19:18 +0530

    Create Fill_Missing_Data.py

---
+++ Pandas/Fill_Missing_Data.py
@@ -0,0 +1,7 @@
+# 2887. Fill Missing Data
+
+import pandas as pd
+
+def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
+    products["quantity"].fillna(0, inplace=True)
+    return products
