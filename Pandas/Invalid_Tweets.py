991bc3a18ca4c57b0babd264bde0845722efd59c
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-10-05 15:45:35 +0530

    Create Invalid_Tweets.py

---
+++ Pandas/Invalid_Tweets.py
@@ -0,0 +1,7 @@
+# 1683. Invalid Tweets
+
+import pandas as pd
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    tweets = tweets[tweets["content"].str.len() > 15]
+    return tweets[["tweet_id"]]
