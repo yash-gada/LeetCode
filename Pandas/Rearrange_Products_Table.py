98d5707ffc631814a4fb8f6dd39b9c118bc3e8dd
yash-gada <79942293+yash-gada@users.noreply.github.com>
2023-10-27 17:22:51 +0530

    Create Rearrange_Products_Table.py

---
+++ Pandas/Rearrange_Products_Table.py
@@ -0,0 +1,7 @@
+# 1795. Rearrange Products Table
+
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    
+    return pd.melt(products, id_vars=["product_id"], value_vars=["store1", "store2", "store3"], var_name="store", value_name="price").dropna()
